f684bfe85aaf18e9567bc7c951b895f569cf82fb
RG <anubis_soft@bk.ru>
2019-04-01 22:02:41 +0300

    Refactor for composer

---
+++ scripts/install.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * src: https://github.com/worksolutions/bitrix-reduce-migrations/blob/master/scripts/install.php
+ */
+
+
+$_SERVER['DOCUMENT_ROOT'] = realpath(dirname(__FILE__) . '/../../../..');
+$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+
+define('NO_KEEP_STATISTIC', true);
+define('NOT_CHECK_PERMISSIONS', true);
+define('CHK_EVENT', true);
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
+require(__DIR__ . '/../install/index.php');
+
+$module = new rg_tauth();
+$module->DoInstall();
+
+echo 'ok';
